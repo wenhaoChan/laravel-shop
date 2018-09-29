75a254012f624eb186660703006fffdd7cf54586
chenwenhao <zerochan1220@163.com>
2018-09-29 17:43:18 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Auth::routes();
 
 Route::get('/email_verified_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
 
+Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+
+// 手动发送验证邮件
+Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+
 Route::group(['middleware' => 'email_verified'], function() {
-    Route::get('/test', function() {
-        return 'Your email is verified';
-    });
+
 });
